cbd7f93c72fda1bbc7c6b618afa8e1dbacdf3a27
it_akumi <it.akumi1011@gmail.com>
2015-11-01 21:32:15 +0900

    modified: dotfilesLink.sh
    Remove 'ln -si ~/dotfiles/.ssh/config ~/.ssh/config'

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -6,7 +6,6 @@ ln -si ~/dotfiles/.zshrc ~/.zshrc
 ln -si ~/dotfiles/.asoundrc ~/.asoundrc
 ln -si ~/dotfiles/.xmonad/xmonad.hs ~/.xmonad/xmonad.hs
 ln -si ~/dotfiles/.xmobarrc ~/.xmobarrc
-ln -si ~/dotfiles/.ssh/config ~/.ssh/config
 ln -si ~/dotfiles/.fehbg ~/.fehbg
 ln -si ~/dotfiles/.terminator/config ~/.config/terminator/config
 ln -si ~/dotfiles/.stalonetrayrc ~/.stalonetrayrc
