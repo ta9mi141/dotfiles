a7fe177ad8827dd6af01dd4f09571f738ac529e4
it_akumi <it.akumi1011@gmail.com>
2015-12-02 13:18:55 +0900

    modified: .xmonad/xmonad.hs
    Make code aligned

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -3,16 +3,16 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.SetWMName
 
 main = xmonad =<< xmobar defaultConfig
-		{ terminal		= myTerminal
-		, modMask		= myModMask
-		, borderWidth		= myBorderWidth
-		, normalBorderColor	= myNormalBorderColor
-		, focusedBorderColor	= myFocusedBorderColor
-		, startupHook		= setWMName "LG3D"
-		}
+        { terminal          =   myTerminal
+        , modMask           =   myModMask
+        , borderWidth       =   myBorderWidth
+        , normalBorderColor =   myNormalBorderColor
+        , focusedBorderColor=   myFocusedBorderColor
+        , startupHook       =   setWMName "LG3D"
+        }
 
-myTerminal	= "terminator"
-myModMask	= mod4Mask	-- Super
-myBorderWidth	= 2
-myNormalBorderColor	= "#000000" --Black
-myFocusedBorderColor	= "#ffff00" --Yellow
+myTerminal              =   "terminator"
+myModMask               =   mod4Mask	-- Super
+myBorderWidth           =   2
+myNormalBorderColor     =   "#000000" --Black
+myFocusedBorderColor    =   "#ffff00" --Yellow
