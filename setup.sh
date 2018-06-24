9f9edf9b026f5abaf65a493341b9b0e5ce68eeab
it_akumi <it.akumi1011@gmail.com>
2018-06-24 13:07:11 +0900

    Add config of sway and termite

--- setup.sh
+++ setup.sh
@@ -1,33 +1,25 @@
-#!/bin/sh
-ln -si $HOME/dotfiles/fehbg $HOME/.fehbg
-ln -si $HOME/dotfiles/gitconfig $HOME/.gitconfig
-ln -si $HOME/dotfiles/vimrc $HOME/.vimrc
-ln -si $HOME/dotfiles/xinitrc $HOME/.xinitrc
-ln -si $HOME/dotfiles/zshrc $HOME/.zshrc
-ln -si $HOME/dotfiles/xmobarrc $HOME/.xmobarrc
-ln -si $HOME/dotfiles/stalonetrayrc $HOME/.stalonetrayrc
-ln -si $HOME/dotfiles/xscreensaver $HOME/.xscreensaver
-ln -si $HOME/dotfiles/spacemacs $HOME/.spacemacs
-
-if [ ! -e $HOME/.xmonad ]; then
-    mkdir $HOME/.xmonad
-    ln -si $HOME/dotfiles/xmonad.hs $HOME/.xmonad/xmonad.hs
-fi
-
-if [ ! -e $HOME/.config/terminator ]; then
-    ln -si $HOME/dotfiles/terminator/ $HOME/.config/terminator
-fi
-
-if [ ! -e $HOME/.config/compton ]; then
-    ln -si $HOME/dotfiles/compton/ $HOME/.config/compton
-fi
+#!/bin/zsh
 
 
-# Set systemd services
+# Copy configs directly under $HOME
+for conf in *(^*/); do
+    ln -sf $HOME/dotfiles/$conf $HOME/.$conf
+done
+
+
+# Copy directories under $HOME/.config
+for conf in config/*; do
+    if [ ! -e $HOME/.config/`basename $conf` ]; then
+        ln -sf $HOME/dotfiles/$conf/ $HOME/.config/`basename $conf`
+    fi
+done
+
+
+# Set systemd user services
 if [ ! -e $HOME/.config/systemd/user ]; then
     mkdir -p $HOME/.config/systemd/user
 fi
 
 for service in $HOME/dotfiles/systemd/user/*.service; do
-    ln -si $service $HOME/.config/systemd/user/
+    ln -sf $service $HOME/.config/systemd/user/
 done
