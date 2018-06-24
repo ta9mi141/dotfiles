0856c2a568fbdf7aa14691b4f72012c82fff91b8
it_akumi <it.akumi1011@gmail.com>
2018-06-24 13:26:26 +0900

    Enable to execute setup.sh regardless of $PWD

--- setup.sh
+++ setup.sh
@@ -2,15 +2,15 @@
 
 
 # Copy configs directly under $HOME
-for conf in *(^*/); do
-    ln -sf $HOME/dotfiles/$conf $HOME/.$conf
+for conf in $HOME/dotfiles/*(^*/); do
+    ln -sf $conf $HOME/.`basename $conf`
 done
 
 
 # Copy directories under $HOME/.config
-for conf in config/*; do
+for conf in $HOME/dotfiles/config/*; do
     if [ ! -e $HOME/.config/`basename $conf` ]; then
-        ln -sf $HOME/dotfiles/$conf/ $HOME/.config/`basename $conf`
+        ln -sf $conf/ $HOME/.config/`basename $conf`
     fi
 done
 
