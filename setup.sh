dab32fc38575f23641569e7d13a7e18109a42356
it_akumi <it.akumi1011@gmail.com>
2019-11-07 22:24:43 +0900

    Move systemd unit files under the config directory

--- setup.sh
+++ setup.sh
@@ -14,13 +14,3 @@ for conf in $HOME/dotfiles/config/*; do
         ln -sf $conf/ $HOME/.config/`basename $conf`
     fi
 done
-
-
-# Set systemd user services
-if [ ! -e $HOME/.config/systemd/user ]; then
-    mkdir -p $HOME/.config/systemd/user
-fi
-
-for service in $HOME/dotfiles/systemd/user/*.service; do
-    ln -sf $service $HOME/.config/systemd/user/
-done
