128233f1bf07fdda6d052f2783a4c4f45ee9832a
Takumi Ishii <ta9mi1shi1@gmail.com>
2021-06-01 11:08:22 +0900

    Use $(command) instead of `command`

--- setup.sh
+++ setup.sh
@@ -4,13 +4,13 @@ set -e
 
 # Copy configs directly under $HOME
 for conf in $HOME/dotfiles/*(^*/); do
-    ln -sf $conf $HOME/.`basename $conf`
+    ln -sf $conf $HOME/.$(basename $conf)
 done
 
 
 # Copy directories under $HOME/.config
 for conf in $HOME/dotfiles/config/*; do
-    if [ ! -e $HOME/.config/`basename $conf` ]; then
-        ln -sf $conf/ $HOME/.config/`basename $conf`
+    if [ ! -e $HOME/.config/$(basename $conf) ]; then
+        ln -sf $conf/ $HOME/.config/$(basename $conf)
     fi
 done
